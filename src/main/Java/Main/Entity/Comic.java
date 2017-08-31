6aff6d353339e51c2c056e00a8dc11a55bede7e4
黄颂钦 <120230406@qq.com>
2017-09-01 06:04:01 +0800

    Initial commit

---
+++ src/main/Java/Main/Entity/Comic.java
@@ -0,0 +1,26 @@
+package main.Java.Main.Entity;
+
+/**
+ * Created by Administrator on 2017/9/1.
+ */
+public class Comic
+{
+    private String Book;
+    private String Author;
+
+    public String getBook() {
+        return Book;
+    }
+
+    public void setBook(String book) {
+        Book = book;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+}
